b35121f18bb6ee5f091c45eda450734fc4a0cce2
Ebrahim Salehipanah <e.salehipanah@gmail.com>
2022-03-15 12:26:52 +0330

    fix

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
 "use strict";
 const box = document.querySelector("#box");
-const hammerBox = new Hammer(box);
-hammerBox.on('panleft', function (ev) {
-    box.classList.add('move-to-left')
-});
-
-
-
-const card = document.querySelector(".card");
-const cardFront  = document.querySelector(".card__front");
-const cardBack  = document.querySelector(".card__back");
-
-
-const hammerCard = new Hammer(card);
-hammerCard.on('panleft panright', function (event) {
-    if(event.deltaX>300 || event.deltaX < -300){
-        console.log(event);
-        cardFront.classList.toggle("flipped")
-        cardBack.classList.toggle("flipped")
-    }
+const hammer = new Hammer(box);
+// console.dir(hammer);
+hammer.on('panleft panright tap press', function (ev) {
+    console.log(ev);
 });
