ca30052730eb7171c4f6983edc7dd121d7654e38
Ebrahim Salehipanah <e.salehipanah@gmail.com>
2022-03-15 12:32:55 +0330

    fix

--- script.js
+++ script.js
@@ -2,15 +2,21 @@
 const box = document.querySelector("#box");
 const hammerBox = new Hammer(box);
 hammerBox.on('panleft', function (ev) {
-    box.classList.add('move-to-left');
+    box.classList.add('move-to-left')
 });
+
+
+
 const card = document.querySelector(".card");
-const cardFront = document.querySelector(".card__front");
-const cardBack = document.querySelector(".card__back");
+const cardFront  = document.querySelector(".card__front");
+const cardBack  = document.querySelector(".card__back");
+
+
 const hammerCard = new Hammer(card);
 hammerCard.on('panleft panright', function (event) {
-    if (event.deltaX > 300 || event.deltaX < -300) {
-        cardFront.classList.toggle("flipped");
-        cardBack.classList.toggle("flipped");
+    if(event.deltaX>300 || event.deltaX < -300){
+        console.log(event);
+        cardFront.classList.toggle("flipped")
+        cardBack.classList.toggle("flipped")
     }
 });
